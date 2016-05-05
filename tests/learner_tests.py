efd787cc19f46dd75ae8449fe2189b7efd4e686d
Frank Blaauw <frank.blaauw@gmail.com>
2016-05-05 15:42:28 +0200

    Added data retrieval and basic file caching

--- tests/learner_tests.py
+++ tests/learner_tests.py
@@ -2,11 +2,11 @@ from nose.tools import *
 import learner
 
 def setup():
-    print "SETUP!"
+    print("SETUP!")
 
 def teardown():
-    print "TEAR DOWN!"
+    print("TEAR DOWN!")
 
 def test_basic():
-    print "I RAN!"
+    print("I RAN!")
 

4b4ebcb3dfaa066ee57c04a39c4d71d3863c6394
Ando Emerencia <ando.emerencia@gmail.com>
2016-06-13 13:29:37 +0200

    fix setup script for new installs

--- setup.sh
+++ setup.sh
@@ -11,7 +11,10 @@ ln -s $input_variable data
 echo 'Linked data directory'
 
 echo 'Seting up Virtual env, assuming Python 3.5'
+brew install python3
+pip3 install --upgrade pip
+pip3 install virtualenv
 pyvenv-3.5 venv
 echo Now run:
 tput setaf 1; echo "source venv/bin/activate"
-tput setaf 1; echo "pip3.5 install -r requirements.txt"
+tput setaf 1; echo "pip3 install -r requirements.txt"

40e194ab8e72fd7a43b90a299d8327657a742646
Frank Blaauw <frank.blaauw@gmail.com>
2016-06-14 16:22:21 +0200

    Implemented diff of diffs strategy

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo 'Running yapf'
+PRE_DIFF=$(git diff)
 yapf -r -i learner
 yapf -r -i tests
+POST_DIFF=$(git diff)
 
-DIFF=$(git diff --name-only | grep '\.py')
+DIFF=$(diff <(echo "$PRE_DIFF") <(echo "$POST_DIFF"))
 if [ "$DIFF" != "" ]
 then
   # Something was wrong

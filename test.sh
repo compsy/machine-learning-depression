bded5eb43296f71ecf48c93428f4ad3175850507
Frank Blaauw <frank.blaauw@gmail.com>
2016-06-14 14:37:38 +0200

    Wip

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 yapf -r -i learner
 DIFF=$(git diff)
-if [ "$DIFF" != ""]
+if [ "$DIFF" != "" ]
 then
   # Something was wrong
+  echo 'Yapf failed:'
+  echo $DIFF
   exit 1
 fi
+echo 'Yapf ran succesfully'

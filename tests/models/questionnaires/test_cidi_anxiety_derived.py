937808a49f1e2f3e239b4c44e74c9a3d7a0ea5e7
Frank Blaauw <frank.blaauw@gmail.com>
2016-06-21 12:38:25 +0200

    Move field fixytue to separate file

--- tests/models/questionnaires/test_cidi_anxiety_derived.py
+++ tests/models/questionnaires/test_cidi_anxiety_derived.py
@@ -18,19 +18,6 @@ class TestCIDIAnxietyDerived:
                                      reader=mock_reader)
         return subject
 
-    @pytest.fixture
-    def mock_get_field(self, request, subject, monkeypatch):
-        my_participant = request.param['participant']
-        field_name = request.param['field']
-        val = request.param['value']
-
-        def fake_get_field(participant, field):
-            assert field_name == field
-            assert my_participant == participant
-            return val
-
-        monkeypatch.setattr(subject, 'get_field', fake_get_field)
-
     def test_init(self, subject):
         # Test if the super class is called with the correct parameters
         assert subject.name == self.name

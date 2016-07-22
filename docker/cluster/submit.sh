a7df2faa5b5c55603f6654c8dbae09ef5d4fa91a
Frank Blaauw <frank.blaauw@gmail.com>
2016-07-22 11:38:33 +0200

    Running on cluster

--- docker/cluster/submit.sh
+++ docker/cluster/submit.sh
@@ -9,6 +9,7 @@ module load Python/3.5.1-foss-2016a
 module load R/3.3.1-foss-2016a
 module load OpenMPI/1.10.2-GCC-4.9.3-2.25-CUDA-7.5.18
 module load matplotlib/1.5.1-foss-2016a-Python-3.5.1
+module load Tk/8.6.4-foss-2016a-libX11-1.6.3
 gitdir="ICPE_machine_learning_workgroup"
 cd $gitdir
 #pip install --user -r requirements.txt

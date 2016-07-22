8f84b3685a3bdb8ade948d24d9019cc72b5ccca4
Frank Blaauw <frank.blaauw@gmail.com>
2016-07-22 09:41:50 +0200

    Running on server

--- docker/cluster/submit.sh
+++ docker/cluster/submit.sh
@@ -8,5 +8,7 @@ module load Python/3.5.1-foss-2016a
 module load R/3.3.1-foss-2016a
 module load OpenMPI/1.10.2-GCC-4.9.3-2.25-CUDA-7.5.18
 gitdir="ICPE_machine_learning_workgroup"
-cd $gitdir/learner
+cd $gitdir
+pip install --user -r requirements.txt
+cd learner
 mpirun python main.py

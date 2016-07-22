8a70f97954f0300c801d292548bc6334e5a87ef2
Frank Blaauw <frank.blaauw@gmail.com>
2016-07-22 10:19:58 +0200

    Running on cluster

--- docker/cluster/submit.sh
+++ docker/cluster/submit.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --time=00:20:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=48
 #SBATCH --job-name=python_cpu
-#SBATCH --cpus-per-task=32
 #SBATCH --mem=8000
 module load Python/3.5.1-foss-2016a
 module load R/3.3.1-foss-2016a
 module load OpenMPI/1.10.2-GCC-4.9.3-2.25-CUDA-7.5.18
 gitdir="ICPE_machine_learning_workgroup"
 cd $gitdir
+#- #SBATCH --cpus-per-task=1
 pip install --user -r requirements.txt
 cd learner
 mpirun python main.py

d2d907ed9d057214f7bb18db75ef1c46930d91a3
Frank Blaauw <frank.blaauw@gmail.com>
2016-07-22 12:04:51 +0200

    Running on cluster

--- docker/cluster/submit.sh
+++ docker/cluster/submit.sh
@@ -8,7 +8,7 @@
 module load Python/3.5.1-foss-2016a
 module load R/3.3.1-foss-2016a
 module load OpenMPI/1.10.2-GCC-4.9.3-2.25-CUDA-7.5.18
-module load matplotlib/1.5.1-foss-2016a-Python-3.5.1
+#module load matplotlib/1.5.1-foss-2016a-Python-3.5.1
 module load Tk/8.6.4-foss-2016a-libX11-1.6.3
 module load libX11/1.6.3-foss-2016a
 gitdir="ICPE_machine_learning_workgroup"
@@ -16,4 +16,5 @@ cd $gitdir
 #pip install --user -r requirements.txt
 cd learner
 echo "Running main.py"
+export MPLBACKEND="agg"
 mpirun python main.py

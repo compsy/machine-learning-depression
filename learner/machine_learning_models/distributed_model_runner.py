c3aced4029550662352ce3dbfecd2df81cdb3d98
Frank Blaauw <frank.blaauw@gmail.com>
2016-07-25 13:03:23 +0200

    Running on cluster

--- learner/machine_learning_models/distributed_model_runner.py
+++ learner/machine_learning_models/distributed_model_runner.py
@@ -13,15 +13,34 @@ class DistributedModelRunner:
         self.models = models
 
     def fabricate_models(self, x, y, x_names, y_names, verbosity):
+        L.info('Fabbing models')
+
         if (self.rank == 0):
-            L.info('Fabbing models')
-            created_models = []
-            for model in self.models:
-                L.info('Model')
-                created_models.append(model(np.copy(x), np.copy(y), x_names, y_names, verbosity))
-            return created_models
+            data = []
+            for i in range(len(self.models)):
+                if i == len(data):
+                    data.append([])
+                data[i].append(self.models[i])
         else:
-            return None
+            data = None
+
+        self.comm.Barrier()
+
+        if (self.rank == 0): L.info('Running %d models on %d nodes' % (len(data), self.size))
+
+        data = self.comm.scatter(data, root=0)
+
+        for model in data[self.rank]:
+            L.info('Training from MPI model runner on node %d' % self.rank)
+            model = model(np.copy(x), np.copy(y), x_names, y_names, verbosity)
+            model.train()
+
+        self.comm.Barrier()
+
+        if self.rank == 0: L.info('!!Trained all models!!')
+
+        return self.comm.gather(data, root=0)
+
 
     def run_calculations(self, fabricated_models):
 
@@ -35,10 +54,10 @@ class DistributedModelRunner:
             data = None
 
         self.comm.Barrier()
-        
-        if(self.rank == 0): L.info('Running %d models on %d nodes' % (len(data), self.size))
 
-        data = self.comm.scatter(data, root = 0)
+        if (self.rank == 0): L.info('Running %d models on %d nodes' % (len(data), self.size))
+
+        data = self.comm.scatter(data, root=0)
 
         for model in data[self.rank]:
             L.info('Training from MPI model runner on node %d' % self.rank)

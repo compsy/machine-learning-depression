a7e951c0745a0bf557a4239dd334061e43a6029f
Frank Blaauw <frank.blaauw@gmail.com>
2016-07-25 17:13:09 +0200

    Running on cluster

--- docker/cluster/submit.sh
+++ docker/cluster/submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=00:20:00
-#SBATCH --nodes=5
+#SBATCH --nodes=6
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
 #SBATCH --mail-type=ALL
@@ -9,10 +9,9 @@
 #SBATCH --mem=8000
 module load Python/3.5.1-foss-2016a
 module load R/3.3.1-foss-2016a
-module load OpenMPI/1.10.2-GCC-4.9.3-2.25-CUDA-7.5.18
 #module load matplotlib/1.5.1-foss-2016a-Python-3.5.1
-module load Tk/8.6.4-foss-2016a-libX11-1.6.3
-module load libX11/1.6.3-foss-2016a
+#module load Tk/8.6.4-foss-2016a-libX11-1.6.3
+#module load libX11/1.6.3-foss-2016a
 gitdir="ICPE_machine_learning_workgroup"
 cd $gitdir
 #pip install --user -r requirements.txt

97f0cca7752e7c22a2f7ab59827348ac89520bac
Frank Blaauw <frank.blaauw@gmail.com>
2016-07-26 15:16:32 +0200

    Running on cluster

--- learner/data_input/spss_reader.py
+++ learner/data_input/spss_reader.py
@@ -10,7 +10,7 @@ class SpssReader:
     def read_file(self, filename):
         rpackages.importr('foreign')
         read_spss = robjects.r['read.spss']
-        data = read_spss("../data/" + filename, to_data_frame=True, use_value_labels=False)
+        data = read_spss("../data/" + filename, to_data_frame=True, use_value_labels=False, reencode='utf-8')
         data = robjects.DataFrame(data)
         data = pandas2ri.ri2py(data)
         return data

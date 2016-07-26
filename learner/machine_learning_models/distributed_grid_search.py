ed310794a4f1a7a7acb290d8e778ab89b6a841da
Frank Blaauw <frank.blaauw@gmail.com>
2016-07-26 14:57:12 +0200

    Running on cluster

--- learner/machine_learning_models/distributed_grid_search.py
+++ learner/machine_learning_models/distributed_grid_search.py
@@ -50,7 +50,7 @@ class DistributedGridSearch:
             if job % self.cpus_per_node == 0 and job != 0:
                 self.queue.put(temp)
                 temp = []
-            current_job = self.merge_dicts(self.param_grid[job])
+            current_job = self.merge_dicts([self.param_grid[job]])
             temp.append(current_job)
 
         # Add an extra job for each node to stop at the end

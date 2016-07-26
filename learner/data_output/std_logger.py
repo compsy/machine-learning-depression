cab0dd75779538ced30b1d4426e2c65136975f9e
Frank Blaauw <frank.blaauw@gmail.com>
2016-07-26 18:07:27 +0200

    Running on cluster

--- learner/data_output/std_logger.py
+++ learner/data_output/std_logger.py
@@ -10,14 +10,14 @@ class L:
         date = time.strftime("%y%m%d-%H%M")
         FORMAT = '%(asctime)-15s -> %(message)s'
         logging.basicConfig(filename='../exports/' + date + '_output.log', format=FORMAT, level=logging.INFO)
-        global logger_hpc
-
+        global logger_on_hpc
+        logger_on_hpc = logger_hpc
         if not logger_hpc:
             L.info('Starting Machine Learning')
 
     @staticmethod
     def info(text, force=False):
-        if logger_hpc and not force:
+        if logger_on_hpc and not force:
             rank = MPI.COMM_WORLD.Get_rank()
             if rank != 0:
                 return

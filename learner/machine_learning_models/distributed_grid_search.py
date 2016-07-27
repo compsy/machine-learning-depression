f33e974153106ce258fa061b7dab9eeca998acb2
Frank Blaauw <frank.blaauw@gmail.com>
2016-07-27 17:24:31 +0200

    Running on cluster

--- learner/machine_learning_models/distributed_grid_search.py
+++ learner/machine_learning_models/distributed_grid_search.py
@@ -113,7 +113,7 @@ class DistributedGridSearch:
         L.info('\t\tSlave: Waiting for data..')
         for task in iter(lambda: self.comm.sendrecv((self.rank,'next'), 0), StopIteration):
             L.info('\t\tSlave: Picking up a task on node %d, task size: %d' % (self.rank, len(task)))
-            model = GridSearchCV(estimator=self.skmodel, param_grid=task, n_jobs=-1, verbose=0, cv=self.cv)
+            model = GridSearchCV(estimator=self.skmodel, param_grid=task, n_jobs=1, verbose=0, cv=self.cv)
             model = model.fit(X=X, y=y)
 
             # only add the best model

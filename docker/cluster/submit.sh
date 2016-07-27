c656de63def53610accd07d149fee96675c675a7
Frank Blaauw <frank.blaauw@gmail.com>
2016-07-27 15:26:56 +0200

    Running on cluster

--- docker/cluster/submit.sh
+++ docker/cluster/submit.sh
@@ -9,10 +9,9 @@
 #SBATCH --mem=8000
 module load Python/3.5.1-foss-2016a
 module load R/3.3.1-foss-2016a
-module load Theano/0.7.0-goolfc-2.7.11-Python-3.4.2
-module load matplotlib/1.5.1-foss-2016a-Python-3.5.1
-module load Tk/8.6.4-foss-2016a-libX11-1.6.3
-module load libX11/1.6.3-foss-2016a
+#module load matplotlib/1.5.1-foss-2016a-Python-3.5.1
+#module load Tk/8.6.4-foss-2016a-libX11-1.6.3
+#module load libX11/1.6.3-foss-2016a
 gitdir="ICPE_machine_learning_workgroup"
 cd $gitdir
 #pip install --user -r requirements.txt

292afdce68f12fe1c1fbb171ab53b4f09fd1a280
Frank Blaauw <frank.blaauw@gmail.com>
2016-07-31 15:48:38 +0200

    Running on cluster

--- learner/data_output/std_logger.py
+++ learner/data_output/std_logger.py
@@ -8,13 +8,13 @@ class L:
 
     @staticmethod
     def setup(logger_hpc):
-        #date = time.strftime("%y%m%d-%H%M")
-        #FORMAT = '%(asctime)-15s -> %(message)s'
-        #logging.basicConfig(filename='../exports/' + date + '_output.log', format=FORMAT, level=logging.INFO)
+        if not logger_hpc:
+            date = time.strftime("%y%m%d-%H%M")
+            FORMAT = '%(asctime)-15s -> %(message)s'
+            logging.basicConfig(filename='../exports/' + date + '_output.log', format=FORMAT, level=logging.INFO)
+            L.info('Starting Machine Learning')
         global logger_on_hpc
         logger_on_hpc = logger_hpc
-        if not logger_hpc:
-            L.info('Starting Machine Learning')
 
     @staticmethod
     def info(text, force=False):
@@ -24,19 +24,19 @@ class L:
                 return
 
         print(text)
-        #logging.info(text)
+        logging.info(text)
 
     @staticmethod
     def br():
         print('')
-        #logging.info('')
+        logging.info('')
 
     @staticmethod
     def debug(text):
         print(text)
-        #logging.debug(text)
+        logging.debug(text)
 
     @staticmethod
     def warn(text):
         warnings.warn(text)
-        #logging.debug(text)
+        logging.debug(text)

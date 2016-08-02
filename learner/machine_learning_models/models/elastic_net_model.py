e193a5a038b12e66e92ed54fa1a0974be0ead1a3
Frank Blaauw <frank.blaauw@gmail.com>
2016-08-02 21:46:39 +0200

    Running on cluster

--- learner/machine_learning_models/models/elastic_net_model.py
+++ learner/machine_learning_models/models/elastic_net_model.py
@@ -12,7 +12,8 @@ class ElasticNetModel(MachineLearningModel):
     def __init__(self, x, y, x_names, y_names, verbosity, grid_search=True):
         super().__init__(x, y, x_names, y_names, model_type='classification')
         self.skmodel = ElasticNet(alpha=1,
-                                  l1_ratio=0.5)
+                                  l1_ratio=0.5,
+                                  max_iter=10000)
 
         if grid_search:
             parameter_grid = {
@@ -24,5 +25,6 @@ class ElasticNetModel(MachineLearningModel):
     def predict_for_roc(self, x_data):
         # TODO: What is the difference between log_proba and proba?
         L.info(self.skmodel.coef_)
+
         L.info(self.skmodel.sparse_coef_)
         return self.skmodel.predict_log_proba(x_data)[:, 1]

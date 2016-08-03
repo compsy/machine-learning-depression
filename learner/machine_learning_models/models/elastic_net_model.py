2f482b9a2321b56469fed3421e0a24fc4e5d410a
Frank Blaauw <frank.blaauw@gmail.com>
2016-08-03 11:53:29 +0200

    Running on cluster

--- learner/machine_learning_models/models/elastic_net_model.py
+++ learner/machine_learning_models/models/elastic_net_model.py
@@ -12,8 +12,7 @@ class ElasticNetModel(MachineLearningModel):
     def __init__(self, x, y, x_names, y_names, verbosity, grid_search=True):
         super().__init__(x, y, x_names, y_names, model_type='classification')
         self.skmodel = ElasticNet(alpha=1,
-                                  l1_ratio=0.5,
-                                  max_iter=1000)
+                                  l1_ratio=0.5)
 
         if grid_search:
             parameter_grid = {

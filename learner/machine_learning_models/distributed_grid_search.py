b4683dfc4a3d565b82a100e84feebd4ffa5ec070
Frank Blaauw <frank.blaauw@gmail.com>
2016-08-07 12:51:40 +0200

    Running on cluster

--- learner/machine_learning_models/distributed_grid_search.py
+++ learner/machine_learning_models/distributed_grid_search.py
@@ -135,7 +135,7 @@ class DistributedGridSearch:
             total_models += len(task)
             grid = [self.param_grid[y] for y in task]
             print('\t\tSlave %d: Received job: %s' % (self.rank, task))
-            model = GridSearchMine(estimator=self.skmodel, param_grid=grid, n_jobs=1, verbose=0, cv=self.cv).fit(X=my_X, y=my_y)
+            model = GridSearchMine(estimator=self.skmodel, param_grid=grid, n_jobs=-1, verbose=0, cv=self.cv).fit(X=my_X, y=my_y)
             models.append((model.best_score_, model.best_estimator_))
             last_run_time = MPI.Wtime() - start
             print('\t\tSlave %d: finished calculation in %0.1f seconds' % (self.rank, last_run_time))

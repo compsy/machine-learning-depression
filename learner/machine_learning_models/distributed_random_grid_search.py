078ad21a46b98fae25f3b9b3a2bae26ceecaf63e
Frank Blaauw <frank.blaauw@gmail.com>
2016-08-07 23:28:01 +0200

    Running on cluster

--- learner/machine_learning_models/distributed_random_grid_search.py
+++ learner/machine_learning_models/distributed_random_grid_search.py
@@ -62,11 +62,11 @@ class DistributedRandomGridSearch:
 
         if self.root:
             best_score, best_model = self.get_best_model(iterations)
+            L.info('\tThese models had %d good models' % (len(iterations)))
+            L.info('\tThe score of the best %s model was %0.3f' % (self.skmodel.given_name, best_score))
         else:
             best_model = None
 
-        L.info('\tThese models had %d good models' % (len(iterations)))
-        L.info('\tThe score of the best %s model was %0.3f' % (self.skmodel.given_name, best_score))
         # Send the model to all clients
         best_model = self.comm.bcast(best_model, root=0)
         return best_model

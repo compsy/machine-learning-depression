4a839fb295e82e3ae2506e41495f7cac3a3fb476
Frank Blaauw <frank.blaauw@gmail.com>
2016-12-06 14:32:30 +0100

    Running on cluster

--- docker/cluster/submit.sh
+++ docker/cluster/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=8:30:00
-#SBATCH --nodes=16
+#SBATCH --time=0:30:00
+#SBATCH --nodes=8
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=23
-####SBATCH --partition=short
+#SBATCH --partition=short
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=peregrine@compsy.nl
 #SBATCH --job-name=python_cpu

d83df19caf939a743a5eb8afec25d09f5b716e89
Frank Blaauw <frank.blaauw@gmail.com>
2016-12-11 14:25:12 +0100

    Running on cluster

--- docker/cluster/submit.sh
+++ docker/cluster/submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --time=0:30:00
-#SBATCH --nodes=32
+#SBATCH --time=1:00:00
+#SBATCH --nodes=20
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=23
 ####SBATCH --partition=short

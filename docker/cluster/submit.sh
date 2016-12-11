ca0c33f3a07ba148a92a5b3a757a6e19e9d73ce9
Frank Blaauw <frank.blaauw@gmail.com>
2016-12-11 12:15:20 +0100

    Running on cluster

--- docker/cluster/submit.sh
+++ docker/cluster/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --time=0:30:00
-#SBATCH --nodes=4
+#SBATCH --nodes=16
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=23
-#SBATCH --partition=short
+####SBATCH --partition=short
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=peregrine@compsy.nl
 #SBATCH --job-name=python_cpu
--- learner/machine_learning_models/machine_learning_model.py
+++ learner/machine_learning_models/machine_learning_model.py
@@ -16,7 +16,7 @@ from learner.machine_learning_models.randomized_search_mine import RandomizedSea
 
 class MachineLearningModel:
 
-    def __init__(self, x, y, x_names, y_names, model_type='models', verbosity=0, hpc=False, n_iter=1000):
+    def __init__(self, x, y, x_names, y_names, model_type='models', verbosity=0, hpc=False, n_iter=10):
         self.x = x
         self.y = y
         self.x_names = x_names
@@ -34,7 +34,7 @@ class MachineLearningModel:
         self.grid_search_type = 'random'
         self.n_iter = n_iter
         if(hpc):
-            self.n_iter = 100
+            self.n_iter = 1000
 
     def remove_missings(self, data):
         imp = Imputer(missing_values='NaN', strategy='mean', axis=0)

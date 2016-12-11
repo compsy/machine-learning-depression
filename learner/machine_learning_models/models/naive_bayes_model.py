37184ea94ae1b86f32b65affd9df402fd6f8f362
Frank Blaauw <frank.blaauw@gmail.com>
2016-12-11 11:14:24 +0100

    Running on cluster

--- learner/machine_learning_models/models/naive_bayes_model.py
+++ learner/machine_learning_models/models/naive_bayes_model.py
@@ -9,10 +9,10 @@ class NaiveBayesModel(MachineLearningModel):
 
 class GaussianNaiveBayesModel(NaiveBayesModel):
     def __init__(self, x, y, x_names, y_names, verbosity, **kwargs):
-        super().__init__(x, y, x_names, y_names, model_type='classification', **kwargs)
+        super().__init__(x, y, x_names, y_names, verbosity = verbosity, model_type='classification', **kwargs)
         self.skmodel = GaussianNB()
 
 class BernoulliNaiveBayesModel(NaiveBayesModel):
     def __init__(self, x, y, x_names, y_names, verbosity, **kwargs):
-        super().__init__(x, y, x_names, y_names, model_type='classification', **kwargs)
+        super().__init__(x, y, x_names, y_names, verbosity = verbosity, model_type='classification', **kwargs)
         self.skmodel = BernoulliNB()

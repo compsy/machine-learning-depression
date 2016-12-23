872b18b23c0d5b8cb92e88c9509d877738996bab
Frank Blaauw <frank.blaauw@gmail.com>
2016-12-23 12:08:39 +0100

    Updated test.sh file

--- test.sh
+++ test.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 echo 'Running yapf'
-#PRE_DIFF=$(git diff)
-#yapf -i -r -e "*venv/*" .
-#POST_DIFF=$(git diff)
+PRE_DIFF=$(git diff)
+yapf -i -r -e "*venv/*" learner
+POST_DIFF=$(git diff)
 
-#DIFF=$(diff <(echo "$PRE_DIFF") <(echo "$POST_DIFF"))
-#if [ "$DIFF" != "" ]
-#then
-  ## Something was wrong
-  #echo 'Yapf failed:'
-  #echo $DIFF
-  #exit 1
-#fi
+DIFF=$(diff <(echo "$PRE_DIFF") <(echo "$POST_DIFF"))
+if [ "$DIFF" != "" ]
+then
+  # Something was wrong
+  echo 'Yapf failed:'
+  echo $DIFF
+  exit 1
+fi
 echo 'Yapf ran succesfully'
 
-#echo 'Running TOX'
-#tox
 echo 'Running py.test'
-cd learner
-echo `pwd`
-py.test --cov=learner ../tests/
+pytest --cov=learner tests/

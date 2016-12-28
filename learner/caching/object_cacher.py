5b3d81d052c874c5efb565e52fca5c50fc35fc5c
Frank Blaauw <frank.blaauw@gmail.com>
2016-12-28 10:27:59 +0100

    Added cacher for the data

---
+++ learner/caching/object_cacher.py
@@ -0,0 +1,28 @@
+import os
+import pickle
+
+
+class ObjectCacher():
+    def __init__(self, directory='cache/'):
+        self.directory = directory
+
+    def get_dirred_file(self, cache_name):
+        return self.directory + cache_name
+
+    def read_cache(self, cache_name):
+        cache_name = self.get_dirred_file(cache_name)
+        if not self.file_available(cache_name, add_dir=False):
+            raise FileNotFoundError('File: ' + cache_name + ' not found!')
+
+        with open(cache_name, 'rb') as input:
+            data = pickle.load(input)
+            return data
+
+    def write_cache(self, data, cache_name):
+        cache_name = self.get_dirred_file(cache_name)
+        with open(cache_name, 'wb') as output:
+            pickle.dump(data, output, pickle.HIGHEST_PROTOCOL)
+
+    def file_available(self, cache_name, add_dir=True):
+        if add_dir: cache_name = self.get_dirred_file(cache_name)
+        return os.path.isfile(cache_name)
---
+++ tests/caching/test_object_cacher.py
@@ -0,0 +1,53 @@
+import pytest
+
+from learner.caching.object_cacher import ObjectCacher
+
+
+class TestObjectCacher():
+
+    @pytest.fixture()
+    def subject(self):
+        subject = ObjectCacher(directory='tests/')
+        return subject
+
+    def test_init_can_add_directory(self):
+        subject = ObjectCacher()
+        assert subject.directory == 'cache/'
+        subject = ObjectCacher('somethingelse')
+
+        assert subject.directory == 'somethingelse'
+
+    def test_raises_when_no_file_found(self, subject):
+        cache_name = 'something_not_existing'
+        with pytest.raises(FileNotFoundError) as err:
+            subject.read_cache(cache_name)
+
+        assert str(err.value) == 'File: tests/' + cache_name + ' not found!'
+
+    def test_can_write_test_data(self, subject):
+        data = {'a':1}
+        subject.write_cache(data, 'data_examples/test_cache')
+        assert subject.file_available('data_examples/test_cache')
+
+    def test_can_read_test_data(self, subject):
+        data = {'a':1}
+        cache_file = 'data_examples/test_cache'
+        subject.write_cache(data, cache_file)
+        result = subject.read_cache(cache_file)
+        assert result == data
+        assert result['a'] == 1
+
+    def test_available_can_check_if_a_file_exists(self, subject):
+        cache_file = 'data_examples/test_cache'
+        assert subject.file_available(cache_file)
+
+        cache_file = 'data_examples/test_cache12313kiofjaosf'
+        assert not subject.file_available(cache_file)
+
+    def test_available_can_check_with_or_without_adding_a_dir(self, subject):
+        cache_file = 'data_examples/test_cache'
+        assert subject.file_available(cache_file)
+        assert not subject.file_available(cache_file, add_dir=False)
+
+        cache_file = 'tests/data_examples/test_cache'
+        assert subject.file_available(cache_file, add_dir=False)

13d15000afd42ad80ac1febded890a24f23fdcaa
Frank Blaauw <frank.blaauw@gmail.com>
2017-06-24 18:52:36 +0200

    Removed the MPI distribution, uncomment it if needed, but we dont want to have the MPI dependency

--- learner/machine_learning_models/distributed_grid_search.py
+++ learner/machine_learning_models/distributed_grid_search.py
@@ -1,147 +1,147 @@
-from sklearn.model_selection import GridSearchCV, ParameterGrid
-import os.path
-from queue import Queue
-from learner.data_output.std_logger import L
-from learner.machine_learning_models.grid_search_mine import GridSearchMine
-import random
-import math
-import numpy as np
+# from sklearn.model_selection import GridSearchCV, ParameterGrid
+# import os.path
+# from queue import Queue
+# from learner.data_output.std_logger import L
+# from learner.machine_learning_models.grid_search_mine import GridSearchMine
+# import random
+# import math
+# import numpy as np
 
 
-class DistributedGridSearch:
+# class DistributedGridSearch:
 
-    def __init__(self, ml_model, estimator, param_grid, cv, cpus_per_node=23):
-        # Number of nodes
-        self.comm = MPI.COMM_WORLD
-        self.size = self.comm.Get_size()
-        self.workers = self.size - 1
-        self.rank = self.comm.Get_rank()
-        self.cpus_per_node = cpus_per_node
-        self.skmodel = estimator
-        self.param_grid = ParameterGrid(param_grid)
-        self.cv = cv
-        self.ml_model = ml_model
+    # def __init__(self, ml_model, estimator, param_grid, cv, cpus_per_node=23):
+        # # Number of nodes
+        # self.comm = MPI.COMM_WORLD
+        # self.size = self.comm.Get_size()
+        # self.workers = self.size - 1
+        # self.rank = self.comm.Get_rank()
+        # self.cpus_per_node = cpus_per_node
+        # self.skmodel = estimator
+        # self.param_grid = ParameterGrid(param_grid)
+        # self.cv = cv
+        # self.ml_model = ml_model
 
-    def fit(self, X, y):
-        self.comm.Barrier()
-        if self.rank == 0:
-            L.info('\tStarting master')
-            return self.master()
-        else:
-            self.slave(X, y)
-            return False
+    # def fit(self, X, y):
+        # self.comm.Barrier()
+        # if self.rank == 0:
+            # L.info('\tStarting master')
+            # return self.master()
+        # else:
+            # self.slave(X, y)
+            # return False
 
-    def create_job_queue(self, shuffle, force_distribute=False):
-        queue = Queue()
-        shuffled_range = list(range(len(self.param_grid)))
-        if shuffle: random.shuffle(shuffled_range)
+    # def create_job_queue(self, shuffle, force_distribute=False):
+        # queue = Queue()
+        # shuffled_range = list(range(len(self.param_grid)))
+        # if shuffle: random.shuffle(shuffled_range)
 
-        work_division = self.cpus_per_node
+        # work_division = self.cpus_per_node
 
-        # If there are more workers than jobs, give each worker a small job. Otherwise just give them x number of jobs,
-        # according to the number of CPUs they have.
-        if self.cpus_per_node * self.workers > len(self.param_grid) and force_distribute:
-            work_division = math.ceil(len(self.param_grid) / self.workers)
+        # # If there are more workers than jobs, give each worker a small job. Otherwise just give them x number of jobs,
+        # # according to the number of CPUs they have.
+        # if self.cpus_per_node * self.workers > len(self.param_grid) and force_distribute:
+            # work_division = math.ceil(len(self.param_grid) / self.workers)
 
-        temp = []
-        for job in shuffled_range:
-            temp.append(job)
-            if (len(temp) == work_division):
-                queue.put(temp)
-                temp = []
+        # temp = []
+        # for job in shuffled_range:
+            # temp.append(job)
+            # if (len(temp) == work_division):
+                # queue.put(temp)
+                # temp = []
 
-        if len(temp) is not 0: queue.put(temp)
+        # if len(temp) is not 0: queue.put(temp)
 
-        # Add an extra job for each node to stop at the end
-        for node in range(self.workers):
-            queue.put(StopIteration)
+        # # Add an extra job for each node to stop at the end
+        # for node in range(self.workers):
+            # queue.put(StopIteration)
 
-        return queue
+        # return queue
 
-    def write_output(self, qsize, wall_time, times):
-        file_name = ('exports/%s.csv' % self.ml_model.given_name)
-        if not os.path.isfile(file_name):
-            with open(file_name, "w") as output_file:
-                output_file.write(
-                    '"nodes", "cpus_per_node", "queue_size", "parameters", "cv", "wall_time", "average", "median", "standard_deviation"\n'
-                )
+    # def write_output(self, qsize, wall_time, times):
+        # file_name = ('exports/%s.csv' % self.ml_model.given_name)
+        # if not os.path.isfile(file_name):
+            # with open(file_name, "w") as output_file:
+                # output_file.write(
+                    # '"nodes", "cpus_per_node", "queue_size", "parameters", "cv", "wall_time", "average", "median", "standard_deviation"\n'
+                # )
 
-        with open(file_name, "a") as output_file:
-            output = (self.size, self.cpus_per_node, qsize, len(self.param_grid), self.cv, wall_time, np.average(times),
-                      np.median(times), np.std(times))
-            output_file.write('%d,%d,%d,%d,%d,%0.2f,%0.2f,%0.2f,%0.2f\n' % output)
+        # with open(file_name, "a") as output_file:
+            # output = (self.size, self.cpus_per_node, qsize, len(self.param_grid), self.cv, wall_time, np.average(times),
+                      # np.median(times), np.std(times))
+            # output_file.write('%d,%d,%d,%d,%d,%0.2f,%0.2f,%0.2f,%0.2f\n' % output)
 
-    def master(self):
-        # Get the queue of jobs to create
-        queue = self.create_job_queue(shuffle=True, force_distribute=True)
-        qsize = queue.qsize()
+    # def master(self):
+        # # Get the queue of jobs to create
+        # queue = self.create_job_queue(shuffle=True, force_distribute=True)
+        # qsize = queue.qsize()
 
-        status = MPI.Status()
-        # running_procs = set()
-        wt = MPI.Wtime()
-        L.info("\tMaster: Starting calculation of %d items, or %d parameters" % (qsize, len(self.param_grid)))
-        times = []
-        while not queue.empty():
-            obj = queue.get()
-            time = self.comm.recv(source=MPI.ANY_SOURCE, status=status)
-            self.comm.send(obj=obj, dest=status.Get_source())
-            L.info("\tMaster: Sending to node %d: %s (%d/%d)" % (status.Get_source(), obj, queue.qsize(), qsize))
-            if time >= 0:
-                times.append(time)
-            #L.info("\tMaster: Queue size: %d/%d (last job by node %d, %d number of configurations, %d nodes)" % (queue.qsize(), qsize, status.Get_source(),len(self.param_grid), self.workers))
+        # status = MPI.Status()
+        # # running_procs = set()
+        # wt = MPI.Wtime()
+        # L.info("\tMaster: Starting calculation of %d items, or %d parameters" % (qsize, len(self.param_grid)))
+        # times = []
+        # while not queue.empty():
+            # obj = queue.get()
+            # time = self.comm.recv(source=MPI.ANY_SOURCE, status=status)
+            # self.comm.send(obj=obj, dest=status.Get_source())
+            # L.info("\tMaster: Sending to node %d: %s (%d/%d)" % (status.Get_source(), obj, queue.qsize(), qsize))
+            # if time >= 0:
+                # times.append(time)
+            # #L.info("\tMaster: Queue size: %d/%d (last job by node %d, %d number of configurations, %d nodes)" % (queue.qsize(), qsize, status.Get_source(),len(self.param_grid), self.workers))
 
-        wt = MPI.Wtime() - wt
+        # wt = MPI.Wtime() - wt
 
-        L.info(
-            '\tQueue is empty, it contained %d items which took %0.2f seconds (%0.2f minutes). Avg: %0.2f, Median: %0.2f, Std: %0.2f'
-            % (qsize, wt, (wt / 60), np.average(times), np.median(times), np.std(times)))
+        # L.info(
+            # '\tQueue is empty, it contained %d items which took %0.2f seconds (%0.2f minutes). Avg: %0.2f, Median: %0.2f, Std: %0.2f'
+            # % (qsize, wt, (wt / 60), np.average(times), np.median(times), np.std(times)))
 
-        self.write_output(qsize, wt, times)
-        models = []
-        models = self.comm.gather(models, root=0)
-        best_model = None
-        best_score = float('-inf')
+        # self.write_output(qsize, wt, times)
+        # models = []
+        # models = self.comm.gather(models, root=0)
+        # best_model = None
+        # best_score = float('-inf')
 
-        L.info('\tWe received %d models' % len(models))
-        good_models = 0
-        for model in models:
-            if len(model) == 0: continue
-            for sub_model in model:
-                good_models += 1
-                if sub_model[0] > best_score:
-                    best_score = sub_model[0]
-                    best_model = sub_model[1]
+        # L.info('\tWe received %d models' % len(models))
+        # good_models = 0
+        # for model in models:
+            # if len(model) == 0: continue
+            # for sub_model in model:
+                # good_models += 1
+                # if sub_model[0] > best_score:
+                    # best_score = sub_model[0]
+                    # best_model = sub_model[1]
 
-        L.info('\tThese models had %d good models' % (good_models))
-        L.info('\tThe score of the best model was %0.3f' % best_score)
-        return best_model
+        # L.info('\tThese models had %d good models' % (good_models))
+        # L.info('\tThe score of the best model was %0.3f' % best_score)
+        # return best_model
 
-    def slave(self, X, y):
-        models = []
+    # def slave(self, X, y):
+        # models = []
 
-        my_X = np.copy(X)
-        my_y = np.copy(y)
-        run_time = 0
-        total_models = 0
-        last_run_time = -1
-        # Ask for work until we receive StopIteration
-        print('\t\tSlave %d: Started my new job, now waiting for data..' % self.rank)
-        for task in iter(lambda: self.comm.sendrecv(last_run_time, 0), StopIteration):
-            start = MPI.Wtime()
-            total_models += len(task)
-            grid = [self.param_grid[y] for y in task]
-            print('\t\tSlave %d: Received job: %s' % (self.rank, task))
-            model = GridSearchMine(
-                estimator=self.skmodel, param_grid=grid, n_jobs=-1, verbose=0, cv=self.cv).fit(
-                    X=my_X, y=my_y)
-            models.append((model.best_score_, model.best_estimator_))
-            last_run_time = MPI.Wtime() - start
-            print('\t\tSlave %d: finished calculation in %0.1f seconds' % (self.rank, last_run_time))
-            run_time += last_run_time
-        if total_models > 0:
-            print('\t\tSlave %d: took %0.0f seconds (avg %0.2f per model)' % (self.rank, run_time,
-                                                                              (run_time / total_models)))
-        else:
-            print('\t\tSlave %d: took %0.0f seconds' % (self.rank, run_time))
-        # Collective report to parent
-        self.comm.gather(sendobj=models, root=0)
+        # my_X = np.copy(X)
+        # my_y = np.copy(y)
+        # run_time = 0
+        # total_models = 0
+        # last_run_time = -1
+        # # Ask for work until we receive StopIteration
+        # print('\t\tSlave %d: Started my new job, now waiting for data..' % self.rank)
+        # for task in iter(lambda: self.comm.sendrecv(last_run_time, 0), StopIteration):
+            # start = MPI.Wtime()
+            # total_models += len(task)
+            # grid = [self.param_grid[y] for y in task]
+            # print('\t\tSlave %d: Received job: %s' % (self.rank, task))
+            # model = GridSearchMine(
+                # estimator=self.skmodel, param_grid=grid, n_jobs=-1, verbose=0, cv=self.cv).fit(
+                    # X=my_X, y=my_y)
+            # models.append((model.best_score_, model.best_estimator_))
+            # last_run_time = MPI.Wtime() - start
+            # print('\t\tSlave %d: finished calculation in %0.1f seconds' % (self.rank, last_run_time))
+            # run_time += last_run_time
+        # if total_models > 0:
+            # print('\t\tSlave %d: took %0.0f seconds (avg %0.2f per model)' % (self.rank, run_time,
+                                                                              # (run_time / total_models)))
+        # else:
+            # print('\t\tSlave %d: took %0.0f seconds' % (self.rank, run_time))
+        # # Collective report to parent
+        # self.comm.gather(sendobj=models, root=0)
--- tests/machine_learning_models/test_distributed_random_grid_search.py
+++ tests/machine_learning_models/test_distributed_random_grid_search.py
@@ -1,126 +1,126 @@
-import inspect
-from unittest.mock import MagicMock, Mock, call
+# import inspect
+# from unittest.mock import MagicMock, Mock, call
 
-from learner.machine_learning_models.distributed_random_grid_search import DistributedRandomGridSearch
-from learner.machine_learning_models import distributed_random_grid_search
-import pytest
-import numpy as np
-import logging
-from mpi4py import MPI
-from random import shuffle
-
-
-class TestDistributedRandomGridSearch():
-    @pytest.fixture()
-    def subject(self):
-        ml_model = 'ml_model'
-        estimator = 'estimator'
-        param_grid = [123]
-        cv = 123
-        n_iter = 321
-        subject = DistributedRandomGridSearch(ml_model, estimator, param_grid, cv, n_iter)
-        return subject
-
-    def test_initialize_should_set_the_correct_parameters(self):
-        ml_model = 'ml_model'
-        estimator = 'estimator'
-        param_grid = [123]
-        cv = 123
-        n_iter = 321
-        subject = DistributedRandomGridSearch(ml_model, estimator, param_grid, cv, n_iter)
-
-        assert subject.skmodel == estimator
-        assert subject.param_grid == param_grid
-        assert subject.cv == cv
-        assert subject.ml_model == ml_model
-        assert subject.iterations == n_iter
-
-    @pytest.mark.skip(reason="no way of currently testing this")
-    def test_initialize_should_set_the_correct_default_parameters(self, monkeypatch, subject):
-        def commworld():
-            return 'test'
-
-        monkeypatch.setattr(MPI, 'COMM_WORLD', commworld)
-
-        ml_model = 'ml_model'
-        estimator = 'estimator'
-        param_grid = [123]
-        cv = 123
-        n_iter = 321
-        subject = DistributedRandomGridSearch(ml_model, estimator, param_grid, cv, n_iter)
-
-        assert subject.comm == commworld()
-
-    # fit
-    @pytest.mark.skip(reason="no way of currently testing this")
-    def test_fit(self, monkeypatch, subject):
-        assert True
-
-    # fit_single_model
-    def test_fit_single_model_should_call_randomized_search_cv(self, monkeypatch, subject):
-        X_data = [[1,2],[3,4]]
-        Y_data = [1,2,3]
-        param_grid = [1,23,4]
-        iterations = 9000
-        randomized_search_return = 'mock_mlmodel_return_value'
-
-        ml_model_mock = Mock(name='MLmodelmock')
-        ml_model_mock.given_name.return_value = 'mock_mlmodel'
-        subject.ml_model = ml_model_mock
-
-        randomized_search_mock = Mock(name='GridSearchRandomMock')
-        monkeypatch.setattr('learner.machine_learning_models.distributed_random_grid_search.RandomizedSearchCV', randomized_search_mock)
-        result = subject.fit_single_model(X_data, Y_data, param_grid, iterations)
-
-        # It should call the randomized search init and the fit of the model.
-        expected_calls = [call(cv=subject.cv, estimator=subject.skmodel, n_iter=iterations, n_jobs=-1,
-                               param_distributions=param_grid, verbose=0),
-                          call().fit(X=X_data, y=Y_data)]
-
-        assert randomized_search_mock.mock_calls == expected_calls
-
-        # It should return the fitted model
-        assert result == randomized_search_mock().fit()
+# from learner.machine_learning_models.distributed_random_grid_search import DistributedRandomGridSearch
+# from learner.machine_learning_models import distributed_random_grid_search
+# import pytest
+# import numpy as np
+# import logging
+# from mpi4py import MPI
+# from random import shuffle
 
 
-    # get_best_model
-    def test_get_best_model_returns_the_best_model(self, monkeypatch, subject):
-        scores = range(10)
-        models = range(10)
-        models = map(lambda a: a * -1, models)
-        models = list(zip(scores, models))
-        shuffle(models)
+# class TestDistributedRandomGridSearch():
+    # @pytest.fixture()
+    # def subject(self):
+        # ml_model = 'ml_model'
+        # estimator = 'estimator'
+        # param_grid = [123]
+        # cv = 123
+        # n_iter = 321
+        # subject = DistributedRandomGridSearch(ml_model, estimator, param_grid, cv, n_iter)
+        # return subject
 
-        result = subject.get_best_model(models)
-        assert type(result) == tuple
-        assert np.array_equal(result, (9, -9))
+    # def test_initialize_should_set_the_correct_parameters(self):
+        # ml_model = 'ml_model'
+        # estimator = 'estimator'
+        # param_grid = [123]
+        # cv = 123
+        # n_iter = 321
+        # subject = DistributedRandomGridSearch(ml_model, estimator, param_grid, cv, n_iter)
 
-    def test_get_best_model_should_be_able_to_deal_with_none_values(self, monkeypatch, subject):
-        scores = range(10)
-        models = range(10)
-        models = list(map(lambda a: a * -1, models))
-        models = list(zip(scores, models))
-        models.append(None)
+        # assert subject.skmodel == estimator
+        # assert subject.param_grid == param_grid
+        # assert subject.cv == cv
+        # assert subject.ml_model == ml_model
+        # assert subject.iterations == n_iter
 
-        shuffle(models)
+    # @pytest.mark.skip(reason="no way of currently testing this")
+    # def test_initialize_should_set_the_correct_default_parameters(self, monkeypatch, subject):
+        # def commworld():
+            # return 'test'
 
-        result = subject.get_best_model(models)
-        assert type(result) == tuple
-        assert np.array_equal(result, (9, -9))
+        # monkeypatch.setattr(MPI, 'COMM_WORLD', commworld)
 
-    def test_get_best_model_should_be_able_to_deal_floats_as_scores(self, monkeypatch, subject):
-        scores = range(10)
+        # ml_model = 'ml_model'
+        # estimator = 'estimator'
+        # param_grid = [123]
+        # cv = 123
+        # n_iter = 321
+        # subject = DistributedRandomGridSearch(ml_model, estimator, param_grid, cv, n_iter)
 
-        scores = list(map(lambda a: a / 10, scores))
-        print(scores)
+        # assert subject.comm == commworld()
 
-        models = range(10)
-        models = list(map(lambda a: - 0.1 * a, models))
-        models = list(zip(scores, models))
-        models.append(None)
+    # # fit
+    # @pytest.mark.skip(reason="no way of currently testing this")
+    # def test_fit(self, monkeypatch, subject):
+        # assert True
 
-        shuffle(models)
+    # # fit_single_model
+    # def test_fit_single_model_should_call_randomized_search_cv(self, monkeypatch, subject):
+        # X_data = [[1,2],[3,4]]
+        # Y_data = [1,2,3]
+        # param_grid = [1,23,4]
+        # iterations = 9000
+        # randomized_search_return = 'mock_mlmodel_return_value'
 
-        result = subject.get_best_model(models)
-        assert type(result) == tuple
-        assert np.array_equal(result, (0.9, -0.9))
+        # ml_model_mock = Mock(name='MLmodelmock')
+        # ml_model_mock.given_name.return_value = 'mock_mlmodel'
+        # subject.ml_model = ml_model_mock
+
+        # randomized_search_mock = Mock(name='GridSearchRandomMock')
+        # monkeypatch.setattr('learner.machine_learning_models.distributed_random_grid_search.RandomizedSearchCV', randomized_search_mock)
+        # result = subject.fit_single_model(X_data, Y_data, param_grid, iterations)
+
+        # # It should call the randomized search init and the fit of the model.
+        # expected_calls = [call(cv=subject.cv, estimator=subject.skmodel, n_iter=iterations, n_jobs=-1,
+                               # param_distributions=param_grid, verbose=0),
+                          # call().fit(X=X_data, y=Y_data)]
+
+        # assert randomized_search_mock.mock_calls == expected_calls
+
+        # # It should return the fitted model
+        # assert result == randomized_search_mock().fit()
+
+
+    # # get_best_model
+    # def test_get_best_model_returns_the_best_model(self, monkeypatch, subject):
+        # scores = range(10)
+        # models = range(10)
+        # models = map(lambda a: a * -1, models)
+        # models = list(zip(scores, models))
+        # shuffle(models)
+
+        # result = subject.get_best_model(models)
+        # assert type(result) == tuple
+        # assert np.array_equal(result, (9, -9))
+
+    # def test_get_best_model_should_be_able_to_deal_with_none_values(self, monkeypatch, subject):
+        # scores = range(10)
+        # models = range(10)
+        # models = list(map(lambda a: a * -1, models))
+        # models = list(zip(scores, models))
+        # models.append(None)
+
+        # shuffle(models)
+
+        # result = subject.get_best_model(models)
+        # assert type(result) == tuple
+        # assert np.array_equal(result, (9, -9))
+
+    # def test_get_best_model_should_be_able_to_deal_floats_as_scores(self, monkeypatch, subject):
+        # scores = range(10)
+
+        # scores = list(map(lambda a: a / 10, scores))
+        # print(scores)
+
+        # models = range(10)
+        # models = list(map(lambda a: - 0.1 * a, models))
+        # models = list(zip(scores, models))
+        # models.append(None)
+
+        # shuffle(models)
+
+        # result = subject.get_best_model(models)
+        # assert type(result) == tuple
+        # assert np.array_equal(result, (0.9, -0.9))
